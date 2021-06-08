2a75afe671c1db915315114fa13564bd4337ee13
mitsuphap <sitanan.mitsuphap@gmail.com>
2021-06-08 23:51:10 +0700

    Set up structure

--- app/src/main/java/com/example/liveat500px/activity/MainActivity.java
+++ app/src/main/java/com/example/liveat500px/activity/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.liveat500px.R;
+import com.example.liveat500px.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer, MainFragment.newInstance())
+                    .commit();
+        }
     }
 }
