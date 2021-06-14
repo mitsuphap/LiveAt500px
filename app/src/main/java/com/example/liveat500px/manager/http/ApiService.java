496dde831414bbcaf8e6bc8a757ad13473994c17
mitsuphap <sitanan.mitsuphap@gmail.com>
2021-06-14 21:16:15 +0700

    Create Data Access Object (DAO)

--- app/src/main/java/com/example/liveat500px/manager/http/ApiService.java
+++ app/src/main/java/com/example/liveat500px/manager/http/ApiService.java
@@ -1,10 +1,12 @@
 package com.example.liveat500px.manager.http;
 
+import com.example.liveat500px.dao.PhotoItemCollectionDao;
+
 import retrofit2.Call;
 import retrofit2.http.POST;
 
 public interface ApiService {
 
     @POST("list")
-    Call<Object> loadPhotoList();
+    Call<PhotoItemCollectionDao> loadPhotoList();
 }
